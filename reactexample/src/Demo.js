a291de41c30b1e463c0f178cbcc1930036ca8e6c
unknown <rohit.raj.bhandari07@gmail.com>
2020-07-23 23:02:59 +0300

    size changes

--- reactexample/src/Demo.js
+++ reactexample/src/Demo.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react';
- 
+import React, { Component } from "react";
 
-const image='https://picsum.photos/330/400';
-let image2='https://picsum.photos/200/300/?blur';
+const image = "https://picsum.photos/330/400";
+let image2 = "https://picsum.photos/200/300/?blur";
 
- class Demo extends Component{ 
-     render(){ 
-         return( 
-         <div className='bg-color'>
-         
-           
-  <h1 contentEditable='true'> Hello {this.props.name}, Welcome to My Blog page...</h1>
-  <div className='img_position'>
-  <img className='pd' src={image} alt='randomImage'/>
-  <img src={image} alt='randomImage'/>
-  <img className='pd' src={image} alt='randomImage'/>
-  </div>
-</div> 
-        )}
-    };
+class Demo extends Component {
+  render() {
+    return (
+      <div className="bg-color">
+        <h1 contentEditable="false">
+          {" "}
+          Hello {this.props.name}, Welcome to My Blog page...
+        </h1>
+        <div className="img_position">
+          <img className="pd" src={image} alt="randomImage" />
+          <img src={image} alt="randomImage" />
+          <img className="pd" src={image} alt="randomImage" />
+        </div>
+      </div>
+    );
+  }
+}
 export default Demo;
